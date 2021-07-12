a1b06f07f247ca8b4f173014576538b985d456d2
Long <longkmttv9@gmail.com>
2021-07-12 14:00:16 +0700

    update

--- ios-opdep/AppDelegate.swift
+++ ios-opdep/AppDelegate.swift
@@ -41,6 +41,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         NFX.sharedInstance().start()
         #endif
         
+        Messaging.messaging().delegate = self
+        
         return true
     }
     
@@ -70,3 +72,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate: MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict:[String: String] = ["token": fcmToken]
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+    
+}
